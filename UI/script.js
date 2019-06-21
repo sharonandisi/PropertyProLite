f36d45a467ba4e60288c9e7ef15748c4b03dfe50
sharonandisi <sharonandisi.28@gmail.com>
2019-06-21 12:11:12 +0300

    build function to open update property window

--- UI/script.js
+++ UI/script.js
@@ -60,3 +60,9 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+//open update listing window
+
+function myFunction() {
+    window.open("update.html");
+}
